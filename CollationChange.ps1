a8a2f436d365255017733ef841e39ce54e88af12
Ørjan Berg <113676242+orjan-berg@users.noreply.github.com>
2023-06-19 16:03:40 +0200

    added $tmpPath

--- CollationChange.ps1
+++ CollationChange.ps1
@@ -3,11 +3,12 @@ if (-not $cred) {
     $cred = Get-Credential -UserName sa -Message 'Password please'
 }
 
+$tmpPath = $env:TEMP
 
-$database = Get-DbaDatabase -SqlInstance $server -SqlCredential $cred -ExcludeSystem | Where-Object {$_.Name -match "collationproblem"}
+$database = Get-DbaDatabase -SqlInstance $server -SqlCredential $cred -ExcludeSystem | Where-Object { $_.Name -match 'collationproblem' }
 
 $diff = Test-DbaDbCollation -SqlInstance $server -SqlCredential $cred -Database $database.Name
-if (-not $diff.IsEqual){
+if (-not $diff.IsEqual) {
     Set-DbaDbState -SqlInstance $server -SqlCredential $cred -Database $database.Name -SingleUser
     Set-DbaDbState -SqlInstance $server -SqlCredential $cred -Database $database.Name -MultiUser    
     Invoke-DbaQuery -SqlInstance $server -SqlCredential $cred -Database $database.Name -Query 'ALTER DATABASE [CollationProblem] COLLATE Danish_Norwegian_CI_AS' 
@@ -16,20 +17,20 @@ if (-not $diff.IsEqual){
 Backup-DbaDatabase -SqlInstance $server -SqlCredential $cred -Database $database.Name -Type Full -CopyOnly
 
 $dbProsess = Get-DbaProcess -SqlInstance $server -SqlCredential $cred -Database $database.Name
-if($dbProsess){
+if ($dbProsess) {
     Stop-DbaProcess -SqlInstance $server -SqlCredential $cred -Database $database.Name -Spid $dbProsess.Spid
 }
 
 Set-DbaDbState -SqlInstance $server -SqlCredential $cred -Database $database.Name -SingleUser
 Set-DbaDbState -SqlInstance $server -SqlCredential $cred -Database $database.Name -MultiUser
-$sql = Invoke-DbaQuery -SqlInstance $server -SqlCredential $cred -Database $database.Name -File 'C:\Scripts\ChangeCollation.sql'
+$sql = Invoke-DbaQuery -SqlInstance $server -SqlCredential $cred -Database $database.Name -File '$tmpPath\ChangeCollation.sql'
 
 $script = $sql.script
-$script -replace "GO...",""
+$script -replace 'GO...',''
 
-$script | Out-File c:\scripts\Collation_alter_script.sql
+$script | Out-File '$tmpPath\Collation_alter_script.sql'
 
-Invoke-DbaQuery -SqlInstance $server -SqlCredential $cred -Database $database.Name -File 'c:\scripts\Collation_alter_script.sql'
+Invoke-DbaQuery -SqlInstance $server -SqlCredential $cred -Database $database.Name -File '$tmpPath\Collation_alter_script.sql'
 
 Restore-DbaDatabase -SqlInstance $server -SqlCredential $cred -Database $database.Name -Path E:\Data\SQL\Backup -WithReplace 
 
