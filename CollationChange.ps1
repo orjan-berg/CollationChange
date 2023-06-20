951f7c3bddcd8645d1dcff6c115b6081e2e4d2cb
Ørjan Berg <113676242+orjan-berg@users.noreply.github.com>
2023-06-20 17:03:46 +0200

    changed to use foreach to handle multiple database

--- CollationChange.ps1
+++ CollationChange.ps1
@@ -1,37 +1,58 @@
+if (-not (Get-Module dbatools -ListAvailable)) {
+    Write-Host ('Installing module dbatools for current user')
+    Install-Module -Scope CurrentUser -Force
+    Write-Host ('Done installing dbatools')
+}
+
+Set-DbatoolsInsecureConnection -SessionOnly
+Start-Transcript -Path .\transcript.txt -UseMinimalHeader
+
 $server = '192.168.50.37'
+$user = 'sa'
 if (-not $cred) {
-    $cred = Get-Credential -UserName sa -Message 'Password please'
+    $cred = Get-Credential -UserName $user -Message 'Password please'
 }
 
-$tmpPath = $env:TEMP
+$database = Get-Content -Path .\databases.csv 
 
-$database = Get-DbaDatabase -SqlInstance $server -SqlCredential $cred -ExcludeSystem | Where-Object { $_.Name -match 'collationproblem' }
+foreach ($db in $database) {
 
-$diff = Test-DbaDbCollation -SqlInstance $server -SqlCredential $cred -Database $database.Name
-if (-not $diff.IsEqual) {
-    Set-DbaDbState -SqlInstance $server -SqlCredential $cred -Database $database.Name -SingleUser
-    Set-DbaDbState -SqlInstance $server -SqlCredential $cred -Database $database.Name -MultiUser    
-    Invoke-DbaQuery -SqlInstance $server -SqlCredential $cred -Database $database.Name -Query 'ALTER DATABASE [CollationProblem] COLLATE Danish_Norwegian_CI_AS' 
-}
+    $diff = Test-DbaDbCollation -SqlInstance $server -SqlCredential $cred -Database $db
+    if (-not $diff.IsEqual) {
+        Write-Host ('Changing database to Singel_User ' + $db)
+        $dbProsess = Get-DbaProcess -SqlInstance $server -SqlCredential $cred -Database $db
+        if ($dbProsess) {
+            Stop-DbaProcess -SqlInstance $server -SqlCredential $cred -Database $db -Spid $dbProsess.Spid
+        }
+        Invoke-DbaQuery -SqlInstance $server -SqlCredential $cred -Database $db -File .\drop_Table_valued_functions.sql
+        Set-DbaDbState -SqlInstance $server -SqlCredential $cred -Database $db -SingleUser 
+        #        Backup-DbaDatabase -SqlInstance $server -SqlCredential $cred -Database $db -Type Full -CopyOnly -CompressBackup 
+        Set-DbaDbState -SqlInstance $server -SqlCredential $cred -Database $db -MultiUser 
+        $sql = 'ALTER DATABASE ' + $db + ' COLLATE Danish_Norwegian_CI_AS' 
+        $result = Invoke-DbaQuery -SqlInstance $server -SqlCredential $cred -Database $db -Query $sql 
+        
+    }
+    Write-Host ('Killing sql connections towards database ' + $db)
+    $dbProsess = Get-DbaProcess -SqlInstance $server -SqlCredential $cred -Database $db
+    if ($dbProsess) {
+        Stop-DbaProcess -SqlInstance $server -SqlCredential $cred -Database $db -Spid $dbProsess.Spid
+    }
+    
+    Set-DbaDbState -SqlInstance $server -SqlCredential $cred -Database $db -SingleUser
+    Set-DbaDbState -SqlInstance $server -SqlCredential $cred -Database $db -MultiUser
+    Write-Host ('Running ChangeCollation.sql in the context of ' + $db)
+    $sql = Invoke-DbaQuery -SqlInstance $server -SqlCredential $cred -Database $db -File .\ChangeCollation.sql
+    
+    $script = $sql.script
+    $script -replace 'GO...',''
+    
+    Write-Host ('Running Collation_alter_script.sql in the context of ' + $db)
+    $script | Out-File .\Collation_alter_script.sql
+    
+    Invoke-DbaQuery -SqlInstance $server -SqlCredential $cred -Database $db -File .\Collation_alter_script.sql
+    Test-DbaDbCollation -SqlInstance $server -SqlCredential $cred -Database $db 
+    Write-Host ('Done with collation change database ' + $db) 
+}    
+Stop-Transcript
+    
 
-Backup-DbaDatabase -SqlInstance $server -SqlCredential $cred -Database $database.Name -Type Full -CopyOnly
-
-$dbProsess = Get-DbaProcess -SqlInstance $server -SqlCredential $cred -Database $database.Name
-if ($dbProsess) {
-    Stop-DbaProcess -SqlInstance $server -SqlCredential $cred -Database $database.Name -Spid $dbProsess.Spid
-}
-
-Set-DbaDbState -SqlInstance $server -SqlCredential $cred -Database $database.Name -SingleUser
-Set-DbaDbState -SqlInstance $server -SqlCredential $cred -Database $database.Name -MultiUser
-$sql = Invoke-DbaQuery -SqlInstance $server -SqlCredential $cred -Database $database.Name -File '$tmpPath\ChangeCollation.sql'
-
-$script = $sql.script
-$script -replace 'GO...',''
-
-$script | Out-File '$tmpPath\Collation_alter_script.sql'
-
-Invoke-DbaQuery -SqlInstance $server -SqlCredential $cred -Database $database.Name -File '$tmpPath\Collation_alter_script.sql'
-
-Restore-DbaDatabase -SqlInstance $server -SqlCredential $cred -Database $database.Name -Path E:\Data\SQL\Backup -WithReplace 
-
-# Copy-DbaDatabase -Source $server -SourceSqlCredential $cred -Destination $server -DestinationSqlCredential $cred -Database CollationProblem -Prefix Newer -BackupRestore -SharedPath E:\Data\SQL\Backup
---
+++ drop_Table_valued_functions.sql
@@ -0,0 +1,11 @@
+
+/****** Object:  UserDefinedFunction dbo.[AccountGroupsDownwardsAll]    Script Date: 20.06.2023 15:38:49 ******/
+DROP FUNCTION IF EXISTS dbo.[AccountGroupsDownwardsAll]
+DROP FUNCTION IF EXISTS dbo.[AccountGroupsDownwardsForAccountGroup]
+DROP FUNCTION IF EXISTS dbo.[VismaReporting_AccountGroupsDownwardsAll]
+DROP FUNCTION IF EXISTS dbo.[VismaReporting_AccountGroupsDownwardsForAccountGroup]
+DROP FUNCTION IF EXISTS dbo.[VismaReporting_NewSplitMultiAcGrFromDfLnIntoRows]
+DROP FUNCTION IF EXISTS dbo.[VismaReporting_SplitMultiAcGrFromDfLnIntoRows]
+DROP FUNCTION IF EXISTS dbo.[VR_ReportStructureFunc_V4]
+GO
+
